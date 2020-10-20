8b28d5dde1c383855a567abd2c0227cc9eff511d
zenix <zenixls2@gmail.com>
2020-10-20 15:32:23 +0900

    fix c++20

--- include/template_common.hpp
+++ include/template_common.hpp
@@ -3,6 +3,7 @@
 #if __cplusplus > 201703L // c++20 code
 
 #include<type_traits>
+#include<array>
 
 #else // __cplusplus
 
@@ -14,6 +15,33 @@ struct type_identity {
 
 // explicit deduction
 template<class... Ts> visit_lambda(Ts...) -> visit_lambda<Ts...>;
+
+namespace {
+template<typename S>
+struct array_proxy {};
+
+template<template<typename, std::size_t> class S, typename T, std::size_t N>
+struct array_proxy<S<T, N>> {
+    std::string value;
+    operator std::array<T, N>() const {
+        std::array<T, N> lhs;
+        size_t end = std::min(N-1, value.size());
+        for (size_t i=0; i<end; ++i) {
+            lhs[i] = value[i];
+        }
+        for (size_t i=end; i<N; ++i) {
+            lhs[i] = 0;
+        }
+        return lhs;
+    }
+};
+
+}
+
+// convert string to specific size of array.
+template<typename T>
+auto to_array(const std::string& v) -> array_proxy<T> { return {v}; }
+
 } // namespace std
 
 #endif // __cplusplus
@@ -37,28 +65,5 @@ constexpr bool always_false = false;
 
 template<class... Args>
 constexpr bool always_true = true;
-
-template<typename S>
-struct array_proxy {};
-
-template<template<typename, std::size_t> class S, typename T, std::size_t N>
-struct array_proxy<S<T, N>> {
-    std::string value;
-    operator std::array<T, N>() const {
-        std::array<T, N> lhs;
-        size_t end = std::min(N-1, value.size());
-        for (size_t i=0; i<end; ++i) {
-            lhs[i] = value[i];
-        }
-        for (size_t i=end; i<N; ++i) {
-            lhs[i] = 0;
-        }
-        return lhs;
-    }
-};
-
-// convert string to specific size of array.
-template<typename T>
-auto to_array(const std::string& v) -> array_proxy<T> { return {v}; }
 
 } // namespace std
